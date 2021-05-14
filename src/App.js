37a9442849c41583172948c0edcfd8a5c3179a96
Rastko <rastkodjurica@yahoo.com>
2021-05-14 13:57:34 -0400

    ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Expenseitem from './components/ExpenseItem';
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>Test One</p>
+				<Expenseitem></Expenseitem>
 			</header>
 		</div>
 	);
