bec3fe316c9e6eccd54987d41ed8c3277329ee9c
Rastko <rastkodjurica@yahoo.com>
2021-05-15 17:45:44 -0400

    Expenses component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Expenseitem from './components/ExpenseItem';
+import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
 	const expenses = [
@@ -25,26 +26,7 @@ function App() {
 	];
 	return (
 		<div>
-			<Expenseitem
-				title={expenses[0].title}
-				amount={expenses[0].amount}
-				date={expenses[0].date}
-			/>
-			<Expenseitem
-				title={expenses[1].title}
-				amount={expenses[1].amount}
-				date={expenses[1].date}
-			/>
-			<Expenseitem
-				title={expenses[2].title}
-				amount={expenses[2].amount}
-				date={expenses[2].date}
-			/>
-			<Expenseitem
-				title={expenses[3].title}
-				amount={expenses[3].amount}
-				date={expenses[3].date}
-			/>
+			<Expenses />
 		</div>
 	);
 }
